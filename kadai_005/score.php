0102a4cc7f974d672bee8aac53129117b1d30b98
specerin <cams.75@icloud.com>
2026-06-03 21:10:00 +0900

    課題提出

---
+++ kadai_005/score.php
@@ -0,0 +1,22 @@
+<?php
+// 変数を作成し値を代入する
+$score1 = 80;
+$score2 = 60;
+$score3 = 55;
+$score4 = 40;
+$score5 = 100;
+$score6 = 25;
+$score7 = 80;
+$score8 = 95;
+$score9 = 30;
+$score10 = 60;
+
+// 合計点を算出
+$total_score = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+
+// 平均点を算出
+$average_score = $total_score / 10;
+
+// 平均点を出力
+echo $average_score;
+?>
